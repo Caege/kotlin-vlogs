9cf188808894b3adad91ea2d6d7184fb656505cb
Caege <mrcapundeadpool@gmail.com>
2024-11-04 21:05:30 +0530

    initial commit

--- app/src/main/java/com/example/intentyt/MainActivity.kt
+++ app/src/main/java/com/example/intentyt/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.intentyt
 
+import android.app.Activity
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
@@ -9,38 +10,121 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat.startActivity
 import com.example.intentyt.ui.theme.IntentYTTheme
 
+const val ZEN = 10
+const val FILEPICKER = 20
+
 class MainActivity : ComponentActivity() {
+	var rules = mutableStateOf<String?>(null)
 	override fun onCreate(savedInstanceState: Bundle?) {
+		fun openZenOfPython() {
+			val intent = Intent(this, SecondActivity::class.java)
+
+			startActivityForResult(intent, ZEN)
+		}
+
+		fun openFilePicker() {
+			val intent = Intent(Intent.ACTION_GET_CONTENT)
+			intent.type = "*/*"
+			startActivityForResult(intent, FILEPICKER)
+		}
+
+
 		super.onCreate(savedInstanceState)
 		enableEdgeToEdge()
 		setContent {
 			IntentYTTheme {
 				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-					Column (modifier = Modifier.fillMaxSize().wrapContentSize()){
-						val context = LocalContext.current
-						Button(onClick = {}, modifier = Modifier.padding(innerPadding)) {
-							Text("Main Activity")
+					val context = LocalContext.current
+					//use lazycolums to allow overflowing content
+					LazyColumn(
+						modifier = Modifier
+							.fillMaxSize()
+							.padding(horizontal = 16.dp)
+							.padding(innerPadding)
+							.wrapContentSize(),
+						horizontalAlignment = Alignment.CenterHorizontally
+					) {
+						//use item functions to manually add Lazy items
+						item {
+							Button(onClick = {}, modifier = Modifier.padding(innerPadding)) {
+								Text("Main Activity")
+							}
 						}
 
-						Button(onClick = { openSecondActivity(context) }) {
-							Text("Launch second activity")
+						item {
+							Button(onClick = { openSecondActivity(context) }) {
+								Text("Launch second activity")
+							}
+						}
+
+						item {
+							Button(onClick = { openGmail(context) }) {
+								Text("Open Gmail")
+							}
+						}
+						item {
+							Button(onClick = { openZenOfPython() }) {
+								Text(text = "Get Zen of Python")
+							}
+						}
+
+
+						item {
+							Button(onClick = { openFilePicker() }) {
+								Text("Open File picker")
+							}
+						}
+
+						item {
+							Spacer(modifier = Modifier.height(50.dp))
+						}
+						if (rules.value != null) {
+							item {
+								Card() {
+									Text(text = rules.value!!, modifier = Modifier.padding(8.dp))
+								}
+							}
+
 						}
 					}
 				}
 			}
+		}
+	}
+
+	override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+		super.onActivityResult(requestCode, resultCode, data)
+
+
+		// handle all sorts of incoming results
+		if (requestCode == ZEN && resultCode == Activity.RESULT_OK) {
+			rules.value = data?.getStringExtra("python")
+
+
+		}
+
+		if (requestCode == FILEPICKER && resultCode == Activity.RESULT_OK) {
+			rules.value = data?.data.toString()
 		}
 	}
 
@@ -52,18 +136,27 @@ class MainActivity : ComponentActivity() {
 //	val intent  = Intent(context,SecondActivity::class.java)
 //	context.startActivity(intent)
 //}
-
-
 //implicit intent
 fun openSecondActivity(context: Context) {
+	try {
+		val intent = Intent(context, SecondActivity::class.java)
+
+		context.startActivity(intent)
+	} catch (e: ActivityNotFoundException) {
+		Toast.makeText(context, "no apps found", Toast.LENGTH_LONG).show()
+	}
+
+}
+
+fun openGmail(context: Context) {
 	try {
 		val intent = Intent(Intent.ACTION_SEND)
 		intent.type = "*/*"
 
 		intent.addCategory(Intent.CATEGORY_DEFAULT)
 //		intent.putExtra("person", "Caege")
-		intent.putExtra(Intent.EXTRA_SUBJECT, "this is a subject")
-		intent.putExtra(Intent.EXTRA_TEXT, "this is the body")
+		intent.putExtra(Intent.EXTRA_SUBJECT, "They are killing us , man")
+		intent.putExtra(Intent.EXTRA_TEXT, "Press the bombo, over")
 		context.startActivity(intent)
 	} catch (e: ActivityNotFoundException) {
 		Toast.makeText(context, "no apps found", Toast.LENGTH_LONG).show()
--- app/src/main/java/com/example/intentyt/SecondActivity.kt
+++ app/src/main/java/com/example/intentyt/SecondActivity.kt
@@ -1,10 +1,14 @@
 package com.example.intentyt
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
@@ -14,6 +18,28 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.Modifier
 import com.example.intentyt.ui.theme.IntentYTTheme
 
+val result: String = """
+			Beautiful is better than ugly.
+			Explicit is better than implicit.
+			Simple is better than complex.
+			Complex is better than complicated.
+			Flat is better than nested.
+			Sparse is better than dense.
+			Readability counts.
+			Special cases aren't special enough to break the rules.
+			Although practicality beats purity.
+			Errors should never pass silently.
+			Unless explicitly silenced.
+			In the face of ambiguity, refuse the temptation to guess.
+			There should be one-- and preferably only one --obvious way to do it.
+			Although that way may not be obvious at first unless you're Dutch.
+			Now is better than never.
+			Although never is often better than right now.
+			If the implementation is hard to explain, it's a bad idea.
+			If the implementation is easy to explain, it may be a good idea.
+			Namespaces are one honking great idea -- let's do more of those!
+		""".trimIndent()
+
 class SecondActivity : ComponentActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
@@ -21,17 +47,38 @@ class SecondActivity : ComponentActivity() {
 		setContent {
 			IntentYTTheme {
 				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-					Button(onClick = {}, modifier = Modifier.padding(innerPadding).fillMaxSize().wrapContentSize()) {
-						Text("Second Activity")
+					Column(
+						modifier = Modifier
+							.padding(innerPadding)
+							.fillMaxSize()
+							.wrapContentSize()
+					) {
+						Button(onClick = {}) {
+							Text("Second Activity")
+						}
+
+						//use callingActivity to check if the activity is started for result or not and display the button accordingly
+						val isStartedForResult = callingActivity != null
+						if (isStartedForResult) {
+							Button(onClick = {
+								val intent = Intent()
+								intent.data = Uri.parse("http://www.youtube.com")
+								intent.putExtra("python", result)
+								setResult(Activity.RESULT_OK, intent)
+								finish()
+							}) {
+								Text(text = "Send Zen of Python")
+							}
+						}
 					}
 				}
 			}
 		}
-		val result: String? = intent.getStringExtra("person")
-
-		if (result != null) {
-			Toast.makeText(this, result, Toast.LENGTH_LONG).show()
-		}
+//		val result: String? = intent.getStringExtra("person")
+//
+//		if (result != null) {
+//			Toast.makeText(this, result, Toast.LENGTH_LONG).show()
+//		}
 
 
 	}
