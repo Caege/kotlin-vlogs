0fd956996217803778c398ef08d367f184df80f7
Caege <mrcapundeadpool@gmail.com>
2024-11-17 20:48:03 +0530

    testing git push

---
+++ app/src/main/java/com/example/cloneeasily/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.cloneeasily
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.cloneeasily.ui.theme.CloneEasilyTheme
+
+class MainActivity : ComponentActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		enableEdgeToEdge()
+		setContent {
+			CloneEasilyTheme {
+				Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+					Greeting(
+
+						modifier = Modifier.padding(innerPadding)
+					)
+				}
+			}
+		}
+	}
+}
+
+@Composable
+fun Greeting( modifier: Modifier = Modifier) {
+	var text by remember {
+		mutableStateOf("Did you clone this project easily?")
+	}
+
+Column(modifier = Modifier
+	.fillMaxSize()
+	.wrapContentSize()) {
+	Card ( modifier = modifier
+		.height(200.dp)
+		.fillMaxWidth()
+		.padding(horizontal = 16.dp)) {
+		Text(
+			text = text, modifier = Modifier
+				.fillMaxSize()
+				.wrapContentSize(), fontWeight = FontWeight.Bold, fontSize = 18.sp,
+
+			)
+	}
+
+	Row(modifier = Modifier
+		.fillMaxWidth()
+		.padding(horizontal = 16.dp), horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+		OutlinedButton(onClick = { text ="Sowwy :( "  }, modifier.weight(1f)) {
+			Text("Nope")
+		}
+
+
+		Button(onClick = { text= "Haha, Great!" }, modifier.weight(1f)) {
+			Text("Yep")
+		}
+	}
+}
+
+
+}
+
